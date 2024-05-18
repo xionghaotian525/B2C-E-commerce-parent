ccc08d39bef3e05c3efd48c6cb3aff51609fecb4
xionghaotian525 <xionghaotian525@gmail.com>
2024-05-18 15:53:20 +0800

    Initial commit

---
+++ B2C-E-commerce-model/src/main/java/com/xionghaotian/dto/product/SkuSaleDto.java
@@ -0,0 +1,12 @@
+package com.xionghaotian.dto.product;
+
+import lombok.Data;
+
+@Data
+public class SkuSaleDto {
+
+	private Long skuId;
+	private Integer num;
+
+}
+
---
+++ B2C-E-commerce-model/src/main/java/com/xionghaotian/vo/product/ProductSkuVO.java
@@ -0,0 +1,30 @@
+package com.xionghaotian.vo.product;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Schema(description = "ProductSkuVO")
+public class ProductSkuVO {
+
+	@Schema(description = "商品编号")
+	private Long sukId;
+
+	@Schema(description = "skuName")
+	private String skuName;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Schema(description = "创建时间")
+	private Date createTime;
+
+	@Schema(description = "缩略图路径")
+	private String thumbImg;
+
+	@Schema(description = "售价")
+	private BigDecimal salePrice;
+
+}
