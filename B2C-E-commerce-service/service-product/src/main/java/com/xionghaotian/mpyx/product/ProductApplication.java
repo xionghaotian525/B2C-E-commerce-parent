c9f2e15bd51edfa78f08920016e8a0b724e27229
sky-take-out <2392334284@qq.com>
2024-06-12 21:37:59 +0800

    前台系统后端--使用spring-cache-启动类添加注解和redis配置类

--- B2C-E-commerce-service/service-product/src/main/java/com/xionghaotian/mpyx/product/ProductApplication.java
+++ B2C-E-commerce-service/service-product/src/main/java/com/xionghaotian/mpyx/product/ProductApplication.java
@@ -3,12 +3,14 @@ package com.xionghaotian.mpyx.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 
 @SpringBootApplication
 @MapperScan("com.xionghaotian.mpyx.product.mapper")
+@EnableCaching
 public class ProductApplication {
 
     public static void main(String[] args) {
