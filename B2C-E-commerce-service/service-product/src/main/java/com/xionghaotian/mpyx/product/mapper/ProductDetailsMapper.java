cca081a18f3b1b743de7df7e7265407dc4f5d460
sky-take-out <2392334284@qq.com>
2024-06-13 21:13:21 +0800

    前台系统后端-商品详情-接口开发-查询ProductDetails表

---
+++ B2C-E-commerce-service/service-product/src/main/java/com/xionghaotian/mpyx/product/mapper/ProductDetailsMapper.java
@@ -0,0 +1,8 @@
+package com.xionghaotian.mpyx.product.mapper;
+
+import com.xionghaotian.entity.product.ProductDetails;
+
+public interface ProductDetailsMapper {
+    ProductDetails getByProductId(Long productId);
+
+}
