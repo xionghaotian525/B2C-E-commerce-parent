c963247ecf25760bd300ccd8bb16e4bbd19ff3a5
sky-take-out <2392334284@qq.com>
2024-06-14 18:27:47 +0800

    前台系统后端-发送短信接口-接口开发测试-配置类添加短信Bean

--- B2C-E-commerce-common/common-service/src/main/java/com/xionghaotian/config/Knife4jConfig.java
+++ B2C-E-commerce-common/common-service/src/main/java/com/xionghaotian/config/Knife4jConfig.java
@@ -18,6 +18,14 @@ import org.springframework.context.annotation.Configuration;
 public class Knife4jConfig {
 
     @Bean
+    public GroupedOpenApi userApi() {      // 创建了一个api接口的分组
+        return GroupedOpenApi.builder()
+                .group("user-api")         // 分组名称
+                .pathsToMatch("/api/**")  // 接口请求路径规则
+                .build();
+    }
+
+    @Bean
     public GroupedOpenApi adminApi() {      // 创建了一个api接口的分组
         return GroupedOpenApi.builder()
                 .group("admin-api")         // 分组名称
